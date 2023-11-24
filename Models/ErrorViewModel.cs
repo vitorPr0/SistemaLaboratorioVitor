000d6b1dabb53d51e2b0b549f2181cc354ca6dec
vitorPr0 <senaivitor899@gmail.com>
2023-11-24 08:32:24 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVFR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
